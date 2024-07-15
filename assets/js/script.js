cdd878a0c01d5a7f4797b3cd99c9c7181efea5f2
Jaque <jaquecl@gmail.com>
2024-07-15 02:11:01 -0400

    se modifica script.js

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(".desplazar").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+$('.scrollup').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+});
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
